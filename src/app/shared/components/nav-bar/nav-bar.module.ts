8c6d50889c4f4a45d211093d69d8a454ef11f052
PJozsef1013 <ptrj1990@gmail.com>
2021-01-14 19:28:54 +0100

    feat: wip: implement navBarComponent

--- src/app/shared/components/nav-bar/nav-bar.module.ts
+++ src/app/shared/components/nav-bar/nav-bar.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavBarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule, RouterModule],
   exports: [NavBarComponent]
 })
 export class NavBarModule {}
